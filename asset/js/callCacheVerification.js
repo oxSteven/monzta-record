28610124e1cd183b0a46efb2dec130f73612ffb7
Kiryi <dev@kiryi.net>
2020-05-16 07:59:14 +0200

    fixed to absolute url

--- asset/js/callCacheVerification.js
+++ asset/js/callCacheVerification.js
@@ -1,4 +1,4 @@
 var xhttp = new XMLHttpRequest();
 
-xhttp.open('GET', 'updateCache/nsSs83BK8kbAvjjreYEP438KALKUgxr3', true);
+xhttp.open('GET', 'https://record.monzta.net/updateCache/nsSs83BK8kbAvjjreYEP438KALKUgxr3', true);
 xhttp.send();
